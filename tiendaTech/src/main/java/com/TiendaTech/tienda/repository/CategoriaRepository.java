23e23278a1eae3dffeb9346893817295911c308c
renzor02 <lruiz70103@ufide.ac.cr>
2026-06-16 20:53:23 -0600

    commit semana 4
    commit semana 4

--- tiendaTech/src/main/java/com/TiendaTech/tienda/repository/CategoriaRepository.java
+++ tiendaTech/src/main/java/com/TiendaTech/tienda/repository/CategoriaRepository.java
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     public List<Categoria> findByActivoTrue();
+ 
 }
  
