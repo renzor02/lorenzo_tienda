d770179578b620aaccb5a01fb5dd18ac88942eb7
renzor02 <lruiz70103@ufide.ac.cr>
2026-07-16 19:45:14 -0600

    Commit semana 7
    Commit semana 7

--- tiendaTech/src/main/java/com/TiendaTech/tienda/SecurityConfig.java
+++ tiendaTech/src/main/java/com/TiendaTech/tienda/SecurityConfig.java
@@ -1,42 +1,51 @@
-package com.tiendaTech.tienda;
+   package com.tiendaTech.tienda;
 
+import com.tiendaTech.tienda.domain.Ruta;
+import com.tiendaTech.tienda.service.RutaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    public static final String[] PUBLIC_URLS = {
-        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
-        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
-    };
-    public static final String[] USUARIO_URLS = {
-        "/facturar/carrito"
-    };
-    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
-        "/producto/listado", "/categoria/listado", "/usuario/listado"
-    };
-    public static final String[] ADMIN_URLS = {
-        "/producto/**", "/categoria/**", "/usuario/**"
-    };
-
+//    public static final String[] PUBLIC_URLS = {
+//        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
+//        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
+//    };
+//    public static final String[] USUARIO_URLS = {
+//        "/facturar/carrito"
+//    };
+//    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+//        "/producto/listado", "/categoria/listado", "/usuario/listado"
+//    };
+//    public static final String[] ADMIN_URLS = {
+//        "/producto/**", "/categoria/**", "/usuario/**"
+//    };
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers(PUBLIC_URLS).permitAll()
-                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
-                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
-                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form // Configuración de formulario de login
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, @Lazy RutaService rutaService)
+            throws Exception {
+
+        var rutas = rutaService.getRutas();
+
+        http.authorizeHttpRequests(requests -> {
+            for (Ruta ruta : rutas) {
+                if (ruta.isRequiereRol()) {
+                    requests.requestMatchers(ruta.getRuta()).hasRole(ruta.getRol().getRol());
+                } else {
+                    requests.requestMatchers(ruta.getRuta()).permitAll();
+                }
+            }
+            requests.anyRequest().authenticated();
+        });
+        http.formLogin(form -> form // Configuración de formulario de login
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/", true)
@@ -62,25 +71,31 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    //Este método será reemplazado la siguiente semana
-    @Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder) {
-        UserDetails juan = User.builder()
-                .username("juan")
-                .password(passwordEncoder.encode("123"))
-                .roles("ADMIN")
-                .build();
-        UserDetails rebeca = User.builder()
-                .username("rebeca")
-                .password(passwordEncoder.encode("456"))
-                .roles("VENDEDOR")
-                .build();
-        UserDetails pedro = User.builder()
-                .username("pedro")
-                .password(passwordEncoder.encode("789"))
-                .roles("USUARIO") // Consistent con tu configuración
-                .build();
-        return new InMemoryUserDetailsManager(juan, rebeca, pedro);
+//    //Este método será reemplazado la siguiente semana
+//    @Bean
+//    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+//        UserDetails juan = User.builder()
+//                .username("juan")
+//                .password(passwordEncoder.encode("123"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails rebeca = User.builder()
+//                .username("rebeca")
+//                .password(passwordEncoder.encode("456"))
+//                .roles("VENDEDOR")
+//                .build();
+//        UserDetails pedro = User.builder()
+//                .username("pedro")
+//                .password(passwordEncoder.encode("789"))
+//                .roles("USUARIO") // Consistent con tu configuración
+//                .build();
+//        return new InMemoryUserDetailsManager(juan, rebeca, pedro);
+//    }
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build,
+            @Lazy PasswordEncoder passwordEncoder,
+            @Lazy UserDetailsService userDetailsService) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
 }
---
+++ tiendaTech/src/main/java/com/TiendaTech/tienda/service/RutaService.java
@@ -0,0 +1,24 @@
+package com.tiendaTech.tienda.service;
+
+import com.tiendaTech.tienda.domain.Ruta;
+import com.tiendaTech.tienda.repository.RutaRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RutaService {
+    
+    private final RutaRepository rutaRepository;
+
+    public RutaService(RutaRepository rutaRepository) {
+        this.rutaRepository = rutaRepository;
+    }
+    
+    @Transactional(readOnly=true)
+    public List<Ruta> getRutas() {
+        return rutaRepository.findAllByOrderByRequiereRolAsc();
+    }
+    
+}
+ 
